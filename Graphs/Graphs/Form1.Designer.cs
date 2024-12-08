e1830161767fc5622f95f836d9a19f80a729145a
StanislavBezb <stasbezborod0@gmail.com>
2024-12-08 18:08:20 +0300

    Добавлены кнопки сохранения, загрузки и перерасчета таблицы

--- Graphs/Graphs/Form1.Designer.cs
+++ Graphs/Graphs/Form1.Designer.cs
@@ -32,7 +32,6 @@
 			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
 			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
 			this.tableLayoutPanel_Tables = new System.Windows.Forms.TableLayoutPanel();
-			this.label_Title = new System.Windows.Forms.Label();
 			this.tableLayoutPanel_Bentonit_La3 = new System.Windows.Forms.TableLayoutPanel();
 			this.label_Bent = new System.Windows.Forms.Label();
 			this.dataGridView_Bentonit_La3 = new System.Windows.Forms.DataGridView();
@@ -46,14 +45,19 @@
 			this.tableLayoutPanel_Kinetika_Sorb_La3 = new System.Windows.Forms.TableLayoutPanel();
 			this.label_Kinetika_Sorb_La3 = new System.Windows.Forms.Label();
 			this.dataGridView_Kinetika_Sorb_La3 = new System.Windows.Forms.DataGridView();
+			this.label_Title = new System.Windows.Forms.Label();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.tableLayoutPanel_Main = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel_Buttons = new System.Windows.Forms.TableLayoutPanel();
+			this.Button_Load = new System.Windows.Forms.Button();
+			this.button_Save = new System.Windows.Forms.Button();
+			this.button_Recalculate = new System.Windows.Forms.Button();
 			this.Column_Kin_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column_Kin_qt_ml = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Column_Kin_qe_qt = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Column_Kin_log_qe_qt = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Column_Kin_t_qt = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Column_Kin_qt_ml = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
 			this.tableLayoutPanel_Tables.SuspendLayout();
 			this.tableLayoutPanel_Bentonit_La3.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView_Bentonit_La3)).BeginInit();
@@ -63,8 +67,9 @@
 			this.splitContainer1.Panel1.SuspendLayout();
 			this.splitContainer1.Panel2.SuspendLayout();
 			this.splitContainer1.SuspendLayout();
-			this.tableLayoutPanel2.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+			this.tableLayoutPanel_Main.SuspendLayout();
+			this.tableLayoutPanel_Buttons.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel_Tables
@@ -81,20 +86,8 @@
 			this.tableLayoutPanel_Tables.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel_Tables.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
 			this.tableLayoutPanel_Tables.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel_Tables.Size = new System.Drawing.Size(524, 547);
+			this.tableLayoutPanel_Tables.Size = new System.Drawing.Size(524, 497);
 			this.tableLayoutPanel_Tables.TabIndex = 0;
-			// 
-			// label_Title
-			// 
-			this.label_Title.AutoSize = true;
-			this.label_Title.Dock = System.Windows.Forms.DockStyle.Top;
-			this.label_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label_Title.Location = new System.Drawing.Point(3, 0);
-			this.label_Title.Name = "label_Title";
-			this.label_Title.Size = new System.Drawing.Size(878, 25);
-			this.label_Title.TabIndex = 0;
-			this.label_Title.Text = "Graphs";
-			this.label_Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			// 
 			// tableLayoutPanel_Bentonit_La3
 			// 
@@ -108,7 +101,7 @@
 			this.tableLayoutPanel_Bentonit_La3.RowCount = 2;
 			this.tableLayoutPanel_Bentonit_La3.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel_Bentonit_La3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel_Bentonit_La3.Size = new System.Drawing.Size(518, 267);
+			this.tableLayoutPanel_Bentonit_La3.Size = new System.Drawing.Size(518, 242);
 			this.tableLayoutPanel_Bentonit_La3.TabIndex = 1;
 			// 
 			// label_Bent
@@ -137,7 +130,7 @@
 			this.dataGridView_Bentonit_La3.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.dataGridView_Bentonit_La3.Location = new System.Drawing.Point(3, 16);
 			this.dataGridView_Bentonit_La3.Name = "dataGridView_Bentonit_La3";
-			this.dataGridView_Bentonit_La3.Size = new System.Drawing.Size(512, 248);
+			this.dataGridView_Bentonit_La3.Size = new System.Drawing.Size(512, 223);
 			this.dataGridView_Bentonit_La3.TabIndex = 1;
 			// 
 			// Column_time
@@ -182,12 +175,12 @@
 			this.tableLayoutPanel_Kinetika_Sorb_La3.Controls.Add(this.label_Kinetika_Sorb_La3, 0, 0);
 			this.tableLayoutPanel_Kinetika_Sorb_La3.Controls.Add(this.dataGridView_Kinetika_Sorb_La3, 0, 1);
 			this.tableLayoutPanel_Kinetika_Sorb_La3.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel_Kinetika_Sorb_La3.Location = new System.Drawing.Point(3, 276);
+			this.tableLayoutPanel_Kinetika_Sorb_La3.Location = new System.Drawing.Point(3, 251);
 			this.tableLayoutPanel_Kinetika_Sorb_La3.Name = "tableLayoutPanel_Kinetika_Sorb_La3";
 			this.tableLayoutPanel_Kinetika_Sorb_La3.RowCount = 2;
 			this.tableLayoutPanel_Kinetika_Sorb_La3.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel_Kinetika_Sorb_La3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel_Kinetika_Sorb_La3.Size = new System.Drawing.Size(518, 268);
+			this.tableLayoutPanel_Kinetika_Sorb_La3.Size = new System.Drawing.Size(518, 243);
 			this.tableLayoutPanel_Kinetika_Sorb_La3.TabIndex = 2;
 			// 
 			// label_Kinetika_Sorb_La3
@@ -207,20 +200,142 @@
 			this.dataGridView_Kinetika_Sorb_La3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView_Kinetika_Sorb_La3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column_Kin_time,
+            this.Column_Kin_qt_ml,
             this.Column_Kin_qe_qt,
             this.Column_Kin_log_qe_qt,
-            this.Column_Kin_t_qt,
-            this.Column_Kin_qt_ml});
+            this.Column_Kin_t_qt});
 			this.dataGridView_Kinetika_Sorb_La3.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.dataGridView_Kinetika_Sorb_La3.Location = new System.Drawing.Point(3, 16);
 			this.dataGridView_Kinetika_Sorb_La3.Name = "dataGridView_Kinetika_Sorb_La3";
-			this.dataGridView_Kinetika_Sorb_La3.Size = new System.Drawing.Size(512, 249);
+			this.dataGridView_Kinetika_Sorb_La3.Size = new System.Drawing.Size(512, 224);
 			this.dataGridView_Kinetika_Sorb_La3.TabIndex = 1;
+			// 
+			// label_Title
+			// 
+			this.label_Title.AutoSize = true;
+			this.label_Title.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label_Title.Location = new System.Drawing.Point(3, 0);
+			this.label_Title.Name = "label_Title";
+			this.label_Title.Size = new System.Drawing.Size(878, 25);
+			this.label_Title.TabIndex = 0;
+			this.label_Title.Text = "Graphs";
+			this.label_Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(3, 28);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel_Tables);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.graph);
+			this.splitContainer1.Size = new System.Drawing.Size(878, 497);
+			this.splitContainer1.SplitterDistance = 524;
+			this.splitContainer1.TabIndex = 1;
+			// 
+			// graph
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.graph.ChartAreas.Add(chartArea2);
+			this.graph.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend2.Name = "Legend1";
+			this.graph.Legends.Add(legend2);
+			this.graph.Location = new System.Drawing.Point(0, 0);
+			this.graph.Name = "graph";
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.graph.Series.Add(series2);
+			this.graph.Size = new System.Drawing.Size(350, 497);
+			this.graph.TabIndex = 0;
+			this.graph.Text = "График";
+			// 
+			// tableLayoutPanel_Main
+			// 
+			this.tableLayoutPanel_Main.ColumnCount = 1;
+			this.tableLayoutPanel_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel_Main.Controls.Add(this.label_Title, 0, 0);
+			this.tableLayoutPanel_Main.Controls.Add(this.splitContainer1, 0, 1);
+			this.tableLayoutPanel_Main.Controls.Add(this.tableLayoutPanel_Buttons, 0, 2);
+			this.tableLayoutPanel_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel_Main.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel_Main.Name = "tableLayoutPanel_Main";
+			this.tableLayoutPanel_Main.RowCount = 3;
+			this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+			this.tableLayoutPanel_Main.Size = new System.Drawing.Size(884, 578);
+			this.tableLayoutPanel_Main.TabIndex = 2;
+			// 
+			// tableLayoutPanel_Buttons
+			// 
+			this.tableLayoutPanel_Buttons.ColumnCount = 3;
+			this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel_Buttons.Controls.Add(this.Button_Load, 0, 0);
+			this.tableLayoutPanel_Buttons.Controls.Add(this.button_Save, 2, 0);
+			this.tableLayoutPanel_Buttons.Controls.Add(this.button_Recalculate, 1, 0);
+			this.tableLayoutPanel_Buttons.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel_Buttons.Location = new System.Drawing.Point(3, 531);
+			this.tableLayoutPanel_Buttons.Name = "tableLayoutPanel_Buttons";
+			this.tableLayoutPanel_Buttons.RowCount = 1;
+			this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel_Buttons.Size = new System.Drawing.Size(878, 44);
+			this.tableLayoutPanel_Buttons.TabIndex = 2;
+			// 
+			// Button_Load
+			// 
+			this.Button_Load.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.Button_Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Button_Load.Location = new System.Drawing.Point(71, 7);
+			this.Button_Load.Name = "Button_Load";
+			this.Button_Load.Size = new System.Drawing.Size(150, 30);
+			this.Button_Load.TabIndex = 0;
+			this.Button_Load.Text = "Загрузить";
+			this.Button_Load.UseVisualStyleBackColor = true;
+			this.Button_Load.Click += new System.EventHandler(this.Button_Load_Click);
+			// 
+			// button_Save
+			// 
+			this.button_Save.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.button_Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button_Save.Location = new System.Drawing.Point(656, 7);
+			this.button_Save.Name = "button_Save";
+			this.button_Save.Size = new System.Drawing.Size(150, 30);
+			this.button_Save.TabIndex = 1;
+			this.button_Save.Text = "Сохранить";
+			this.button_Save.UseVisualStyleBackColor = true;
+			this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+			// 
+			// button_Recalculate
+			// 
+			this.button_Recalculate.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.button_Recalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button_Recalculate.Location = new System.Drawing.Point(363, 7);
+			this.button_Recalculate.Name = "button_Recalculate";
+			this.button_Recalculate.Size = new System.Drawing.Size(150, 30);
+			this.button_Recalculate.TabIndex = 2;
+			this.button_Recalculate.Text = "Пересчитать";
+			this.button_Recalculate.UseVisualStyleBackColor = true;
+			this.button_Recalculate.Click += new System.EventHandler(this.button_Recalculate_Click);
 			// 
 			// Column_Kin_time
 			// 
 			this.Column_Kin_time.HeaderText = "обр\\врем";
 			this.Column_Kin_time.Name = "Column_Kin_time";
+			// 
+			// Column_Kin_qt_ml
+			// 
+			this.Column_Kin_qt_ml.HeaderText = "qt, μмоль/г";
+			this.Column_Kin_qt_ml.Name = "Column_Kin_qt_ml";
 			// 
 			// Column_Kin_qe_qt
 			// 
@@ -237,67 +352,12 @@
 			this.Column_Kin_t_qt.HeaderText = "t\\qt";
 			this.Column_Kin_t_qt.Name = "Column_Kin_t_qt";
 			// 
-			// Column_Kin_qt_ml
-			// 
-			this.Column_Kin_qt_ml.HeaderText = "qt, μмоль/г";
-			this.Column_Kin_qt_ml.Name = "Column_Kin_qt_ml";
-			// 
-			// splitContainer1
-			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(3, 28);
-			this.splitContainer1.Name = "splitContainer1";
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel_Tables);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add(this.chart1);
-			this.splitContainer1.Size = new System.Drawing.Size(878, 547);
-			this.splitContainer1.SplitterDistance = 524;
-			this.splitContainer1.TabIndex = 1;
-			// 
-			// tableLayoutPanel2
-			// 
-			this.tableLayoutPanel2.ColumnCount = 1;
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Controls.Add(this.label_Title, 0, 0);
-			this.tableLayoutPanel2.Controls.Add(this.splitContainer1, 0, 1);
-			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
-			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-			this.tableLayoutPanel2.RowCount = 2;
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(884, 578);
-			this.tableLayoutPanel2.TabIndex = 2;
-			// 
-			// chart1
-			// 
-			chartArea2.Name = "ChartArea1";
-			this.chart1.ChartAreas.Add(chartArea2);
-			this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-			legend2.Name = "Legend1";
-			this.chart1.Legends.Add(legend2);
-			this.chart1.Location = new System.Drawing.Point(0, 0);
-			this.chart1.Name = "chart1";
-			series2.ChartArea = "ChartArea1";
-			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-			series2.Legend = "Legend1";
-			series2.Name = "Series1";
-			this.chart1.Series.Add(series2);
-			this.chart1.Size = new System.Drawing.Size(350, 547);
-			this.chart1.TabIndex = 0;
-			this.chart1.Text = "chart1";
-			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(884, 578);
-			this.Controls.Add(this.tableLayoutPanel2);
+			this.Controls.Add(this.tableLayoutPanel_Main);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.tableLayoutPanel_Tables.ResumeLayout(false);
@@ -312,9 +372,10 @@
 			this.splitContainer1.Panel2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
 			this.splitContainer1.ResumeLayout(false);
-			this.tableLayoutPanel2.ResumeLayout(false);
-			this.tableLayoutPanel2.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+			this.tableLayoutPanel_Main.ResumeLayout(false);
+			this.tableLayoutPanel_Main.PerformLayout();
+			this.tableLayoutPanel_Buttons.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -336,14 +397,18 @@
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Kinetika_Sorb_La3;
 		private System.Windows.Forms.Label label_Kinetika_Sorb_La3;
 		private System.Windows.Forms.DataGridView dataGridView_Kinetika_Sorb_La3;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Main;
+		private System.Windows.Forms.DataVisualization.Charting.Chart graph;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Buttons;
+		private System.Windows.Forms.Button Button_Load;
+		private System.Windows.Forms.Button button_Save;
+		private System.Windows.Forms.Button button_Recalculate;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column_Kin_time;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column_Kin_qt_ml;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column_Kin_qe_qt;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column_Kin_log_qe_qt;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column_Kin_t_qt;
-		private System.Windows.Forms.DataGridViewTextBoxColumn Column_Kin_qt_ml;
-		private System.Windows.Forms.SplitContainer splitContainer1;
-		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
 	}
 }
 
--- Graphs/Graphs/Form1.cs
+++ Graphs/Graphs/Form1.cs
@@ -14,8 +14,11 @@ namespace Graphs
 {
 	public partial class Form1 : Form
 	{
-		//функция заполнения таблицы из csv файла
-		public void readCSVtoDataGridView_Bentonit_La3(string path_to_csv, DataGridView table)
+		OpenFileDialog openFileDialog;
+		SaveFileDialog saveFileDialog;
+
+		//функция начального заполнения таблицы из csv файла (чтобы они не пустовали при открытии)
+		public void Fill_DataGridView_Bentonit_La3(string path_to_csv, DataGridView table)
 		{
 			//чтение данных
 			string[] rows = File.ReadAllLines(path_to_csv);
@@ -63,7 +66,7 @@ namespace Graphs
 			}
 		}
 
-		public void readCSVtoDataGridView_Kinetika_Sorb_La3(string path_to_csv, DataGridView table)
+		public void Fill_DataGridView_Kinetika_Sorb_La3(string path_to_csv, DataGridView table)
 		{
 			//чтение данных
 			string[] rows = File.ReadAllLines(path_to_csv);
@@ -107,13 +110,53 @@ namespace Graphs
 			}
 		}
 
+		public void readCSVtoDataGridView_Bentonit_La3(string path_to_csv, DataGridView table)
+		{
+			//очищаем таблицу
+			table.Rows.Clear();
+			table.Refresh();
+
+			//чтение данных
+			string[] rows = File.ReadAllLines(path_to_csv);
+
+			for (int i = 1; i < rows.Length; i++)
+			{
+				string[] columns = rows[i].Split('|');
+				table.Rows.Add();
+				for (int j = 0; j < columns.Length; j++)
+				{
+					table.Rows[i - 1].Cells[j].Value = columns[j];
+				}
+			}
+		}
+
+		public void readCSVtoDataGridView_Kinetika_Sorb_La3(string path_to_csv, DataGridView table)
+		{
+			//очищаем таблицу
+			table.Rows.Clear();
+			table.Refresh();
+
+			//чтение данных
+			string[] rows = File.ReadAllLines(path_to_csv);
+
+			for (int i = 1; i < rows.Length; i++)
+			{
+				string[] columns = rows[i].Split('|');
+				table.Rows.Add();
+				for (int j = 0; j < columns.Length; j++)
+				{
+					table.Rows[i - 1].Cells[j].Value = columns[j];
+				}
+			}
+		}
+
 		public void createGraphics()
 		{
 			for (int i = 0; i < 6; i++)
 			{
 				double x = double.Parse(this.dataGridView_Kinetika_Sorb_La3["Column_Kin_time", i].Value.ToString());
 				double y = double.Parse(this.dataGridView_Kinetika_Sorb_La3["Column_Kin_log_qe_qt", i].Value.ToString());
-				this.chart1.Series[0].Points.AddXY(x, y);
+				this.graph.Series[0].Points.AddXY(x, y);
 			}
 		}
 		public Form1()
@@ -126,14 +169,127 @@ namespace Graphs
 			this.splitContainer1.Panel2Collapsed = true;
 
 			//заполнение таблицы из файла
-			readCSVtoDataGridView_Bentonit_La3(path_to_data, this.dataGridView_Bentonit_La3);
+			Fill_DataGridView_Bentonit_La3(path_to_data, this.dataGridView_Bentonit_La3);
 
-			readCSVtoDataGridView_Kinetika_Sorb_La3(path_to_data_Kin, this.dataGridView_Kinetika_Sorb_La3);
+			Fill_DataGridView_Kinetika_Sorb_La3(path_to_data_Kin, this.dataGridView_Kinetika_Sorb_La3);
 
 			this.splitContainer1.Panel2Collapsed = false;
 			createGraphics();
 
 
+		}
+
+		private void Button_Load_Click(object sender, EventArgs e)
+		{
+			openFileDialog = new OpenFileDialog();
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				readCSVtoDataGridView_Bentonit_La3(openFileDialog.FileName, this.dataGridView_Bentonit_La3);
+			}
+		}
+
+		private void button_Recalculate_Click(object sender, EventArgs e)
+		{
+			int rowCount = this.dataGridView_Bentonit_La3.RowCount;
+			DataGridView table = this.dataGridView_Bentonit_La3;
+			//заполнение ячеек по формулам
+
+			//заполнение таблицы Benronit_La3
+			//заполнение столбца "С,мкг/мл"
+			for (int i = 0; i < rowCount - 1; i++)
+			{
+				table["Column_C", i].Value = (double.Parse(table["Column_A", i].Value.ToString()) / 0.0158).ToString(); //CultureInfo.InvariantCulture чтобы распознавалась ".", а не ","
+
+			}
+			//заполнение столбца "qt, мкг/г"
+			double column_c_0 = double.Parse(table["Column_C", 0].Value.ToString());
+			for (int i = 1; i < rowCount - 1; i++)
+			{
+				double column_c_i = double.Parse(table["Column_C", i].Value.ToString());
+				double column_m_r_i = double.Parse(table["Column_m_r", i].Value.ToString());
+				table["Column_qt", i].Value = (column_c_0 - column_c_i) * 20 / column_m_r_i;
+
+			}
+			//заполнение столбца "qt, μмоль/г"
+			for (int i = 1; i < rowCount - 1; i++)
+			{
+				table["Column_qt_ml", i].Value = double.Parse(table["Column_qt", i].Value.ToString()) / 1355;
+
+			}
+			//заполнение столбца "%"
+			column_c_0 = double.Parse(table["Column_C", 0].Value.ToString());
+			for (int i = 1; i < rowCount - 1; i++)
+			{
+				double column_c_i = double.Parse(table["Column_C", i].Value.ToString());
+				table["Column_proc", i].Value = (column_c_0 - column_c_i) / column_c_0 * 100;
+
+			}
+
+			rowCount = this.dataGridView_Kinetika_Sorb_La3.RowCount;
+			table = this.dataGridView_Kinetika_Sorb_La3;
+			//заполнение таблицы Kinetika_Sorb_La3
+			//заполнение столбца "обр\врем"
+			for (int i = 0; i < rowCount - 1; i++)
+			{
+				table["Column_Kin_time", i].Value = this.dataGridView_Bentonit_La3["Column_time", i].Value;
+			}
+			//зваполнение столбца "qt, μмоль/г"
+			for (int i = 0; i < rowCount - 1; i++)
+			{
+				table["Column_Kin_qt_ml", i].Value = this.dataGridView_Bentonit_La3["Column_qt_ml", i].Value;
+			}
+
+			//заполнение столбца "qe-qt"
+			double Qe1 = 0.362;
+			for (int i = 0; i < rowCount - 1; i++)
+			{
+				double Kin_qt_ml_i = double.Parse(table["Column_Kin_qt_ml", i].Value.ToString());
+				table["Column_Kin_qe_qt", i].Value = Qe1 - Kin_qt_ml_i;
+			}
+
+			//заполнение столбца "log(qe-qt)"
+			for (int i = 0; i < rowCount - 1; i++)
+			{
+				table["Column_Kin_log_qe_qt", i].Value = Math.Log(double.Parse(table["Column_Kin_qe_qt", i].Value.ToString()), 10);
+			}
+
+			//заполнение столбца "t\qt"
+			for (int i = 1; i < rowCount - 1; i++)
+			{
+				double column_Kin_time_i = double.Parse(table["Column_Kin_time", i].Value.ToString());
+				double column_Kin_qt_ml_i = double.Parse(table["Column_Kin_qt_ml", i].Value.ToString());
+				table["Column_Kin_t_qt", i].Value = column_Kin_time_i / column_Kin_qt_ml_i;
+			}
+
+			//перечерчиваем график
+			this.graph.Series[0].Points.Clear();
+			createGraphics();
+		}
+
+		private void button_Save_Click(object sender, EventArgs e)
+		{
+			saveFileDialog = new SaveFileDialog();
+			if (saveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				StreamWriter sw = new StreamWriter(saveFileDialog.FileName);
+				//string[] strings = new string[this.dataGridView_Bentonit_La3.RowCount];
+				string title = "time|m|A|C|qt|qtm|%";
+				sw.WriteLine(title);
+				//strings[0] = title;
+				for (int i = 0; i < this.dataGridView_Bentonit_La3.RowCount - 1; i++)
+				{
+					for (int j = 0; j < this.dataGridView_Bentonit_La3.ColumnCount; j++)
+					{
+						sw.Write(this.dataGridView_Bentonit_La3.Rows[i].Cells[j].Value);
+						if (j != this.dataGridView_Bentonit_La3.ColumnCount - 1)
+						{
+							sw.Write("|");
+						}
+					}
+					sw.WriteLine();
+				}
+				sw.Close();
+			}
 		}
 	}
 }
